4006322e350b7e0618becca6d0ce2815972b2e8b
DaoZu-Yishizhidao <nicholas.yy@foxmail.com>
2026-04-08 21:36:28 +0800

    博客更新: 2026-04-08 21:36:28

---
+++ scripts/writing-date-override.js
@@ -0,0 +1,26 @@
+const moment = require('moment');
+
+hexo.extend.filter.register('before_post_render', function(data) {
+  if (data.writing_date) {
+    const newDate = moment(data.writing_date);
+    if (newDate.isValid()) {
+      // 赋值为 moment 对象，而不是 Date 对象
+      data.date = newDate;
+      data.updated = newDate;
+    }
+  }
+  return data;
+});
+
+hexo.extend.filter.register('after_generate', function() {
+  const posts = this.locals.get('posts');
+  posts.forEach(post => {
+    if (post.writing_date) {
+      const newDate = moment(post.writing_date);
+      if (newDate.isValid()) {
+        post.date = newDate;
+        post.updated = newDate;
+      }
+    }
+  });
+});
